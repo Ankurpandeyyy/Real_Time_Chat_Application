50fad1c788f6c7a97fb12c2e1cd88c758ba67404
ankur <ankurkumar0924@gmail.com>
2025-06-20 00:32:57 +0530

    4th commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,7 +24,7 @@ app.use(cookieParser()); //parsing cookies or grabing value from cookies
 // ];
 app.use(cors({
     //origin:"http://localhost:5173",
-    origin:"https://real-time-chat-application-tau-orcin.vercel.app/",
+    origin:"https://real-time-chat-application-tau-orcin.vercel.app",
     credentials: true
 }))
 app.use("/api/auth",authRoutes);
